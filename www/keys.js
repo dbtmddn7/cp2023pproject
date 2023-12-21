1377a254c43e6001ba377b000481e0130e5b3261
dbtmddn7 <k19rs140@st.kyusan-u.ac.jp>
2023-12-21 09:15:00 +0900

    データベースに接続するためのappKeyとclientKeyの設定、cssを利用したインターフェース設計

--- www/keys.js
+++ www/keys.js
@@ -1,3 +1,3 @@
 // This is a JavaScript file
-var appKey = "d39cda425f289faa67ab01edd039f4696f0225429fb3bd2a5bfdcce878a8078f";
-var clientKey = "815a502c532384628183f65907db405d63ed96dec2e1a14ab343630b294a3e3f";
+let appKey = "d39cda425f289faa67ab01edd039f4696f0225429fb3bd2a5bfdcce878a8078f";
+let clientKey = "815a502c532384628183f65907db405d63ed96dec2e1a14ab343630b294a3e3f";
